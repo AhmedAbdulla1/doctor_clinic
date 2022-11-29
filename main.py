cbc450f4886b87efee8d541cd09c928c28fc8438
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2022-11-30 01:45:11 +0200

    1:45

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Doctor:
     def tick(self):
         if self.exist_patient:
             self.time_remaining = self.time_remaining - 1
-            if self.time_remaining == 0:
+            if self.time_remaining <= 0:  # التركاية هنا ==>
                 self.exist_patient = False
         return
 
@@ -28,9 +28,6 @@ class Patient:
     def __init__(self, buildTime):
         self.buildTime = buildTime
         self.patientAge = random.randrange(20, 61)
-
-    def getBuildTime(self):
-        return self.buildTime
 
     def getPatientAge(self):
         return self.patientAge
@@ -43,8 +40,11 @@ def simulation(numOfMinutes, patientTime):
     doctor = Doctor(patientTime)
     patient_queue = Queue()
     waiting_list_times = []
+    num_of_patients = 0
     for currentTime in range(numOfMinutes):
-        if random.randrange(1, 25) == 24:
+        r = random.randrange(0, 6)
+        if r == 5:
+            num_of_patients = num_of_patients + 1  # number of patient
             patient = Patient(currentTime)
             patient_queue.put(patient)
         if doctor.isFree() and not patient_queue.empty():
@@ -52,11 +52,19 @@ def simulation(numOfMinutes, patientTime):
             waiting_list_times.append(next_patient.waitingTime(currentTime))
             doctor.start(next_patient)
         doctor.tick()
+    print("Number of patient in this day is ", num_of_patients)
     average_wait = sum(waiting_list_times) / len(waiting_list_times)
-    print("Average Wait ", average_wait, " secs", patient_queue.qsize(), " tasks remaining.")
+    print("Average Wait ", average_wait, " min and ", patient_queue.qsize(), " patient remaining.")
 
 
 # start this program
+print("Start\nin this part patient time with doctor = (Age/5)\n")
 for i in range(10):
-    simulation(240, 5)
+    simulation(240, 5)  # in this part patient time with doctor = (Age/5)
+    print()
+print("finished this part\n")
+print("In this part patient time with doctor = (Age/10)\n")
+for i in range(10):
+    simulation(240, 10)  # in this part patient time with doctor = (Age/10)
+    print()
 print("finished")
